297c7df5e2fee9d1760c74c5ab319dffc720836d
Eunggu Yun <yuneg11@gmail.com>
2019-11-19 11:43:49 +0900

    Add MPI communicator initialization

--- lud2.cpp
+++ lud2.cpp
@@ -15,12 +15,12 @@ int main(int argc, char *argv[]) {
     struct timeval start;
     struct timeval end;
 
-    int n = atoi(argv[1]);
-    int s = atoi(argv[2]);
+    int mat_len = atoi(argv[1]);
+    int seed = atoi(argv[2]);
 
-    double *matrixA = new double[n * n];
-    double *matrixLU = new double[n * n];
-    double *matrixB = new double[n * n];
+    double *matrixA = new double[mat_len * mat_len];
+    double *matrixLU = new double[mat_len * mat_len];
+    double *matrixB = new double[mat_len * mat_len];
 
     if (MPI_Init(NULL, NULL) != 0) {
         printf("Error: MPI_Init failed\n");
@@ -29,24 +29,50 @@ int main(int argc, char *argv[]) {
 
     // MPI Region
     {
+        // Initialize MPI Communicator
+        int rank, comm_size;
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
+
+        int comm_per_line = (int)sqrt(comm_size);
+        MPI_Comm *row_comm = new MPI_Comm[comm_per_line-1];
+        MPI_Comm *col_comm = new MPI_Comm[comm_per_line-1];
+        int *row_rank = new int[comm_per_line-1];
+        int *col_rank = new int[comm_per_line-1];
+        int row_order = rank / comm_per_line;
+        int col_order = rank % comm_per_line;
+        int calc_order = (row_order < col_order ? row_order : col_order);
+
+        for (int i = 0; i < comm_per_line-1; i++) {
+            row_rank[i] = (col_order >= i && row_order >= i ? row_order - i : -1);
+            col_rank[i] = (col_order >= i && row_order >= i ? col_order - i : -1);
+        }
+
+        MPI_Comm_split(MPI_COMM_WORLD, rank % comm_per_line, rank, &row_comm[0]);
+        MPI_Comm_split(MPI_COMM_WORLD, rank / comm_per_line, rank, &col_comm[0]);
+        for (int i = 1; i <= calc_order+1 && i < comm_per_line-1; i++) {
+            MPI_Comm_split(row_comm[i-1], (row_rank[i-1] == 0 ? 0 : 1), row_rank[i-1], &row_comm[i]);
+            MPI_Comm_split(col_comm[i-1], (col_rank[i-1] == 0 ? 0 : 1), col_rank[i-1], &col_comm[i]);
+        }
+
         // Build Matrix
-        srand(s);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                matrixA[i * n + j] = matrixLU[i * n + j] = (double)rand();
+        srand(seed);
+        for (int i = 0; i < mat_len; i++) {
+            for (int j = 0; j < mat_len; j++) {
+                matrixA[i * mat_len + j] = matrixLU[i * mat_len + j] = (double)rand();
             }
         }
 
         // LU Decomposition
         gettimeofday(&start, NULL);
 
-        for (int k = 0; k < n; k++) {
-            for (int i = k + 1; i < n; i++) {
-                matrixLU[i * n + k] /= matrixLU[k * n + k];
+        for (int k = 0; k < mat_len; k++) {
+            for (int i = k + 1; i < mat_len; i++) {
+                matrixLU[i * mat_len + k] /= matrixLU[k * mat_len + k];
             }
-            for (int i = k + 1; i < n; i++) {
-                for (int j = k + 1; j < n; j++) {
-                    matrixLU[i * n + j] -= matrixLU[i * n + k] * matrixLU[k * n + j];
+            for (int i = k + 1; i < mat_len; i++) {
+                for (int j = k + 1; j < mat_len; j++) {
+                    matrixLU[i * mat_len + j] -= matrixLU[i * mat_len + k] * matrixLU[k * mat_len + j];
                 }
             }
         }
@@ -55,23 +81,23 @@ int main(int argc, char *argv[]) {
         printf("LU Decomposition: %f s\n",(end.tv_sec-start.tv_sec)+(float)(end.tv_usec-start.tv_usec) / 1000000.0);
 
         // Reconstruct Matrix
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < mat_len; i++) {
+            for (int j = 0; j < mat_len; j++) {
                 int min_idx = min(i - 1, j);
-                matrixB[i * n + j] = (j >= i ? matrixLU[i * n + j] : 0);
+                matrixB[i * mat_len + j] = (j >= i ? matrixLU[i * mat_len + j] : 0);
                 for (int k = 0; k <= min_idx; k++) {
-                    matrixB[i * n + j] += matrixLU[i * n + k] * matrixLU[k * n + j];
+                    matrixB[i * mat_len + j] += matrixLU[i * mat_len + k] * matrixLU[k * mat_len + j];
                 }
-                matrixB[i * n + j] = matrixB[i * n + j];
+                matrixB[i * mat_len + j] = matrixB[i * mat_len + j];
             }
         }
 
         // Calculate Matrix Difference
         double diff = 0;
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < mat_len; j++) {
             double row_sum = 0;
-            for (int i = 0; i < n; i++) {
-                row_sum += (matrixA[i * n + j] - matrixB[i * n + j]) * (matrixA[i * n + j] - matrixB[i * n + j]);
+            for (int i = 0; i < mat_len; i++) {
+                row_sum += (matrixA[i * mat_len + j] - matrixB[i * mat_len + j]) * (matrixA[i * mat_len + j] - matrixB[i * mat_len + j]);
             }
             diff += sqrt(row_sum);
         }
